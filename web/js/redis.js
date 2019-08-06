2a56573238b08079c1f7d39d00c38a32f1ba67c5
bohglab <bohglab@bohglab>
2019-08-06 13:20:46 -0700

    Parse big int for older browsers

--- web/js/redis.js
+++ web/js/redis.js
@@ -244,7 +244,24 @@ function parsePayload(buffer, idx) {
     idx += 2;
   } else if (lenPayload == 127) {
     // Large payload size
-    lenPayload = new DataView(buffer, idx).getBigUint64();
+    if (DataView.prototype.getBigUint64 === undefined) {
+      DataView.prototype.getBigUint64 = function(byteOffset, littleEndian) {
+        // split 64-bit number into two 32-bit parts
+        byteOffset = byteOffset || 0;
+        littleEndian = littleEndian || false;
+        const left =  this.getUint32(byteOffset, littleEndian);
+        const right = this.getUint32(byteOffset+4, littleEndian);
+
+        // combine the two 32-bit values
+        const combined = littleEndian? left + 2**32*right : 2**32*left + right;
+
+        if (!Number.isSafeInteger(combined))
+          console.warn(combined, 'exceeds MAX_SAFE_INTEGER. Precision may be lost');
+
+        return combined;
+      }
+    }
+    lenPayload = Number(new DataView(buffer, idx).getBigUint64());
     idx += 8;
   }
 
